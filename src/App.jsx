314fda14f29f5773cf27a32bb7ac21cfa21071e9
Luis Acevedo <luisemilio.acevedo@globant.com>
2024-02-27 18:56:11 -0500

    css terminado

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,23 @@ import './App.css'
 function App() {
   return (
     <>
-      <article class="todo">
-        <section className="imagen">
-          <div className="imagen-principal">
-            <img src="https://static.wikia.nocookie.net/logopedia/images/7/76/Ol%C3%A9_1996.svg" alt="logo ole" className="logo" />
-          </div>
-        </section>
+      <article className="todo">
+        <div className="imagen-principal">
+          <img src="https://static.wikia.nocookie.net/logopedia/images/7/76/Ol%C3%A9_1996.svg" alt="logo ole" className="logo" />
+        </div>
         <section className="separador">
           <div className="texto">
             <p>28 de noviembre de 2000, en el Estadio Nacional de Tokio.</p>
           </div>
         </section>
         <section className="titulo">
-          <div className="titulo-div">
-            <div className="semiti">
-              <p>campeón intercontinental <strong>2000</strong></p>
-            </div>
-            <p>Boca</p>
+          <div className="semiti">
+            <p>campeón intercontinental <strong>2000</strong></p>
           </div>
+          <p className='color'>Boca</p>
         </section>
         <section className="footer">
-          <div className="footer-div">
-            <p>el mundo en tus manos</p>
-          </div>
+          <p className="footer-texto">el mundo en tus manos</p>
         </section>
       </article>
     </>
